13e1aedf69f9b630c640622583040c967f7c0068
Zulh <zulhfreelancer@gmail.com>
2018-07-08 04:35:49 +0800

    Create a receiver inside 'deck' type to print cards

--- section-3/cards/deck.go
+++ section-3/cards/deck.go
@@ -1,6 +1,15 @@
 package main
 
+import "fmt"
+
 // Create a new type of 'deck' which is a slice of strings.
 // In object oriented, it's like we are extending String class
 // here into 'deck' class. Note: Go doesn't has class.
 type deck []string
+
+// Create receiver with 'print' as the name
+func (d deck) print() {
+  for i, card := range d {
+    fmt.Println(i, card)
+  }
+}
--- section-3/cards/main.go
+++ section-3/cards/main.go
@@ -1,16 +1,13 @@
 package main
 
-import "fmt"
-
 func main() {
   cards := deck{"Ace of Diamonds", newCard()}
 
   // pushing a new card into the slice
   cards = append(cards, "Six of Spades")
 
-  for i, card := range cards {
-    fmt.Println(i, card)
-  }
+  // call receiver in deck.go
+  cards.print()
 }
 
 func newCard() string {

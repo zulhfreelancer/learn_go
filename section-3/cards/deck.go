eadab8433acd3e335dd271771ad1f9739a744506
Zulh <zulhfreelancer@gmail.com>
2018-07-08 04:46:34 +0800

    Rename variables to more clearer names (and add some comments)

--- section-3/cards/deck.go
+++ section-3/cards/deck.go
@@ -7,9 +7,13 @@ import "fmt"
 // here into 'deck' class. Note: Go doesn't has class.
 type deck []string
 
-// Create receiver with 'print' as the name
-func (d deck) print() {
-  for i, card := range d {
+// Create receiver with 'print' as the name.
+// Any variables of type 'deck' in our project will get access
+// to this 'print' method.
+// The 'self' can be anything. By convention, it should be just 'd'.
+// Note: there is no such thing called 'self' or 'this' in Go.
+func (self deck) print() {
+  for i, card := range self {
     fmt.Println(i, card)
   }
 }

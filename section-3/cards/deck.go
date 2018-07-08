ebc2a6fff5ec848735385938a8daad4bb08b0109
Zulh <zulhfreelancer@gmail.com>
2018-07-09 02:49:27 +0800

    Add function to save file to disk

--- section-3/cards/deck.go
+++ section-3/cards/deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "strings"
+  "io/ioutil"
+)
 
 // Create a new type of 'deck' which is a slice of strings.
 // In object oriented, it's like we are extending String class
@@ -40,4 +44,23 @@ func deal(d deck, handSize int) (deck, deck) {
   // First part takes from first element up to 'handSize' EXCLUDING 'handSize'.
   // Second part takes from 'handSize' to the last element.
   return d[:handSize], d[handSize:]
+}
+
+// Helper 'method' to convert deck to string (comma separated).
+func (d deck) toString() string {
+  // convert deck to slice of strings
+  sliceOfStrings := []string(d)
+  // join all elements in that slice of strings
+  return strings.Join(sliceOfStrings, ",")
+}
+
+// Save a file to disk.
+// The 'error' is a type. The 'WriteFile' returns 'error',
+// so we need to implement it here, just in case something is wrong.
+// The 0666 means anyone can read and write this file.
+func (d deck) saveToFile(filename string) error {
+  deckInString := d.toString()
+  sliceOfBytes := []byte(deckInString)
+  // we 'return' here so that it will return 'error' (if we get one)
+  return ioutil.WriteFile(filename, sliceOfBytes, 0666)
 }
--- section-3/cards/main.go
+++ section-3/cards/main.go
@@ -1,13 +1,8 @@
 package main
 
-import "fmt"
+// import "fmt"
 
 func main() {
   cards := newDeck()
-
-  hand, remainingCards := deal(cards, 5)
-
-  hand.print()
-  fmt.Println("-------------------")
-  remainingCards.print()
+  cards.saveToFile("myCards.txt")
 }

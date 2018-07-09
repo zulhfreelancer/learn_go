4950c770ca57926251615d45f1518e5d276c4ab4
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2018-07-09 17:22:20 +0000

    Test the first and the last card

--- section-3/cards/deck_test.go
+++ section-3/cards/deck_test.go
@@ -10,4 +10,12 @@ func TestNewDeck(t *testing.T) {
     // The '%v' will take and render the value of next argument
     t.Errorf("Expected deck length of 16, but got %v", len(d))
   }
+
+  if d[0] != "Ace of Spades" {
+    t.Errorf("Expected first card is Ace of Spades, but got %v", d[0])
+  }
+
+  if d[len(d) - 1] != "Four of Clubs" {
+    t.Errorf("Expected first card is Four of Clubs, but got %v", d[len(d) - 1])
+  }
 }

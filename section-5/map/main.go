139f8f92f7b1551704f301164f7efdec5b05931b
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2018-07-15 15:20:41 +0000

    Add and delete key pairs inside map

--- section-5/map/main.go
+++ section-5/map/main.go
@@ -3,10 +3,21 @@ package main
 import "fmt"
 
 func main() {
-  colors := map[string]string{
-    "white": "#fff",
-    "black": "#000",
-  }
+  // First way of writing map.
+  // colors := map[string]string{
+  //   "white": "#fff",
+  //   "black": "#000",
+  // }
+
+  // Second way of writing map.
+  // var colors map[string]string
+
+  // Third way of writing map.
+  colors := make(map[string]string)
+  // Add key pair to the map.
+  colors["white"] = "#fff"
+  // Delete key pair from the map.
+  delete(colors, "white")
 
   fmt.Println(colors)
 }

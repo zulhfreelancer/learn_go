a0dfe71e6e6785dffdca8f159b49beddffaec9d9
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2018-07-17 17:28:20 +0000

    Hello channels and routines

--- section-7/channels/main.go
+++ section-7/channels/main.go
@@ -12,17 +12,29 @@ func main() {
     "http://amazon.com",
   }
 
+  // Create a channel.
+  // We gonna share string in the channel.
+  c := make(chan string)
+
   for _, link := range links {
-    checkLink(link)
+    go checkLink(link, c)
+  }
+
+  for i := 0; i < len(links); i++ {
+    fmt.Println(<- c)
   }
 }
 
-func checkLink(link string) {
-  // We don't care about response, we care about error here.
+func checkLink(link string, c chan string) {
+  // We don't care about response (first argument), we care about error here.
   _, err := http.Get(link)
+
   if err != nil {
     fmt.Println(link, "might be down")
+    c <- "Might me down I think"
     return // that's all, no more code to execute
   }
+
   fmt.Println(link, "is up!")
+  c <- "Yeap, it's up!"
 }

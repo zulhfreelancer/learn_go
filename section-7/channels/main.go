315e1989e20845cb0b1bbe16e8cd97b6e35ae022
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2018-07-18 08:05:59 +0000

    Add sleep inside the Go Routine

--- section-7/channels/main.go
+++ section-7/channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
   "fmt"
   "net/http"
+  "time"
 )
 
 func main() {
@@ -25,7 +26,19 @@ func main() {
   // go checkLink line: then run child routine, passing the link that we've just received
   // This loop syntax is equivalent with previous loop syntax.
   for link := range c {
-    go checkLink(link, c)
+    // Create an anonymous function and execute it straight away.
+    // To reduce confusion, the `_link` is the `link` passed at the end of this function.
+    go func(_link string) {
+      // Pause it for 5 seconds.
+      time.Sleep(5 * time.Second)
+
+      // Execute the next child routine.
+      checkLink(_link, c)
+
+      // Execute this anonymous function straight away in form of Go Routine,
+      // passing `link` (from the `range` line) as argument.
+      // That is same as calling a `nonAnonymousFunction(link)`.
+    }(link)
   }
 }
 
